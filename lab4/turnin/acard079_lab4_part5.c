e017bcc34d9d61c8f384651d95eeb272cae5488a
Ana Cardenas Beltran <acard079@ucr.edu>
2021-04-16 02:03:01 -0700

    Completed part 1

---
+++ lab4/turnin/acard079_lab4_part5.c
@@ -0,0 +1,85 @@
+/*	Author: acard079
+ *  Partner(s) Name: 
+ *	Lab Section: 21
+ *	Assignment: Lab #4  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum states {SM1_START, SM1_PB0, SM1_PB0RELEASE, SM1_PB1, SM1_PB1RELEASE} state;
+
+void TickFct()
+{
+	switch(state) {
+		case SM1_START:
+			state = SM1_PB0;
+			break;
+		case SM1_PB0:
+			if(PINA & 0x01){
+				state = SM1_PB0;
+			}else{
+				state = SM1_PB0RELEASE;
+			}
+			break;
+		case SM1_PB0RELEASE:
+			if(PINA & 0x01){
+				state = SM1_PB1;
+			}else{
+				state = SM1_PB0RELEASE;
+			}
+			break;
+		case SM1_PB1:
+			if(PINA & 0x01){
+				state = SM1_PB1;
+			}else{
+				state = SM1_PB1RELEASE;
+			}
+			break;
+		case SM1_PB1RELEASE:
+			if(PINA & 0x01){
+				state = SM1_PB0;
+			}else{
+				state = SM1_PB1RELEASE;
+			}
+			break;
+		default:
+			state = START;
+	}
+	switch(state){
+		case SM1_START:
+			PORTB = 0x01;
+			break;
+		case SM1_PB0:
+			PORTB = 0x01;
+			break;
+		case SM1_PB0RELEASE:
+			PORTB = 0x01;
+			break;
+		case SM1_PB1:
+			PORTB = 0x02;
+			break;
+		case SM1_PB1RELEASE:
+			PORTB = 0x02;
+			break;
+	}
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; // PORTA is input
+	DDRB = 0xFF; PORTB = 0x00; //PORTB is output
+
+    /* Insert your solution below */
+    while (1) {
+	    TickFct();
+
+
+    }
+    return 1;
+}
